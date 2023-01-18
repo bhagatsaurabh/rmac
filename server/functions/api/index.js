65898a0d4fed6674cf4ef9145ea5efcc9c711ae2
Saurabh Bhagat <saurabhbhagat98die@gmail.com>
2023-01-18 23:59:30 +0530

    added initial docs

--- server/functions/api/index.js
+++ server/functions/api/index.js
@@ -5,11 +5,14 @@ import { errorHandler } from '../middleware/error-handler.js';
 import { getRegister } from './register.js';
 import { getCommand, postCommand } from './command.js';
 import { getUpdate } from './update.js';
+import { serve, setup } from 'swagger-ui-express';
+import swaggerDoc from '../swagger.json';
 
 const getServer = (context) => {
     const server = express();
 
     server.use(bodyParser.json());
+    server.use('/api-docs', serve, setup(swaggerDoc));
     server.use((req, _res, next) => {
         req.context = context;
         next();
