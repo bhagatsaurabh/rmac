5e8e173f55958bc67cd133c6e875b1ffbf78142a
Saurabh Bhagat <saurabhbhagat98die@gmail.com>
2023-01-24 23:45:19 +0530

    added heartbeat for plain websockets

--- bridge-server/src/servers/socket.js
+++ bridge-server/src/servers/socket.js
@@ -7,25 +7,18 @@ import { onClose, onError } from '../events/listeners.js';
 const server = http.createServer(staticServer);
 const socketServer = new WebSocketServer({ server });
 
-/* const heartbeat = (socket) => { socket.isAlive = true; };
-const interval = setInterval(() => {
-    socketServer.clients.forEach((socket) => {
-        if (socket.isAlive === false) {
-            return socket.terminate();
-        }
-
-        socket.isAlive = false;
-        socket.ping(() => ping(socket));
-    })
-}, process.env.MAX_HEARTBEAT_THRESHOLD); */
-
 socketServer.on('connection', (socket) => {
-    /* socket.isAlive = true;
-    socket.on('pong', () => heartbeat(socket)); */
+    socket.isAlive = true;
+    socket.on('pong', () => heartbeat(socket));
 
     socket.on('close', () => onClose(socket));
     socket.on('error', (err) => onError(socket, err));
     socket.on('message', (rawData) => {
+        if (rawData.toString() === '?') {
+            heartbeat(socket);
+            return;
+        }
+
         const message = parse(rawData);
 
         if (message.event === 'identity') {
@@ -35,5 +28,21 @@ socketServer.on('connection', (socket) => {
         }
     });
 });
+socketServer.on('close', () => {
+    clearInterval(pingTimer);
+});
+
+const heartbeat = (socket) => { socket.isAlive = true; };
+const pingTimer = setInterval(() => {
+    socketServer.clients.forEach((socket) => {
+        if (socket.isAlive === false) {
+            return socket.terminate();
+        }
+
+        socket.isAlive = false;
+        if (socket.type === 'host') socket.ping();
+        else if (socket.type === 'console') socket.send('?');
+    })
+}, process.env.MAX_HEARTBEAT_THRESHOLD);
 
 export default server;
