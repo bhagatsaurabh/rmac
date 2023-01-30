d69012dd9b105877c7fa3513c9945e7f5866b2b9
Saurabh Bhagat <saurabhbhagat98die@gmail.com>
2023-01-30 23:28:12 +0530

    console: fixed theme 'auto'

--- console/src/store/preferences/index.js
+++ console/src/store/preferences/index.js
@@ -2,85 +2,67 @@ import { mutationKeys, themes } from '@/store/constants';
 
 const state = () => ({
   theme: themes.SYSTEM,
-  systemTheme: themes.LIGHT,
 });
 
 const mutations = {
   [mutationKeys.SET_THEME]: (state, theme) => {
+    state.theme = '';
     state.theme = theme;
-  },
-  [mutationKeys.SET_SYSTEM_THEME]: (state, theme) => {
-    state.systemTheme = theme;
-  },
-  [mutationKeys.SET_PREFERENCES]: (state, preferences) => {
-    state.theme = preferences.theme;
-    state.systemTheme = preferences.systemTheme;
   },
 };
 
 const actions = {
-  setTheme({ getters, state, commit, dispatch }, newTheme) {
-    const oldTheme = state.theme;
-
-    if (typeof newTheme !== 'symbol' || !Object.values(themes).includes(newTheme)) {
+  async setTheme({ getters: { getThemeClass, systemTheme }, commit, dispatch }, newTheme) {
+    if (!Object.values(themes).includes(newTheme)) {
       return;
     }
 
+    const themeClass = getThemeClass(newTheme === themes.SYSTEM ? systemTheme : newTheme);
+    document.documentElement.dataset.theme = themeClass;
+
     if (newTheme === themes.SYSTEM) {
-      document.documentElement.className = '';
-    } else if (oldTheme === themes.SYSTEM) {
-      document.documentElement.classList.add(getters.getClassName(newTheme));
+      document.documentElement.className = themeClass;
     } else {
-      document.documentElement.classList.replace(
-        getters.getClassName(),
-        getters.getClassName(newTheme)
-      );
+      document.documentElement.classList.replace(getThemeClass(), themeClass);
     }
 
     commit(mutationKeys.SET_THEME, newTheme);
-    dispatch('updatePreferences');
+    await dispatch('updatePreferences');
   },
-  setSystemTheme({ commit }, theme) {
-    if (typeof theme !== 'symbol' || !Object.values(themes).includes(theme)) {
-      return;
-    }
-
-    commit(mutationKeys.SET_SYSTEM_THEME, theme);
-  },
-  updatePreferences({ getters }) {
-    const serialized = JSON.stringify(getters.getSerializableState());
+  updatePreferences({ getters: { serializableState } }) {
+    const serialized = JSON.stringify(serializableState);
     localStorage.setItem('user-preferences', serialized);
   },
-  loadPreferences({ getters, commit }) {
+  async loadPreferences({ dispatch }) {
     const storedPreferences = JSON.parse(localStorage.getItem('user-preferences'));
 
     if (!storedPreferences) {
-      commit(mutationKeys.SET_SYSTEM_THEME, getters.getSystemTheme());
+      await dispatch('setTheme', themes.SYSTEM);
     } else {
-      storedPreferences.theme = Object.values(themes).find(
-        (theme) => theme.description === storedPreferences.theme
-      );
-      storedPreferences.systemTheme = getters.getSystemTheme();
+      storedPreferences.theme =
+        Object.values(themes).find((theme) => theme === storedPreferences.theme) || themes.SYSTEM;
 
-      commit(mutationKeys.SET_PREFERENCES, storedPreferences);
+      await dispatch('setTheme', storedPreferences.theme);
     }
   },
 };
 
 const getters = {
-  getClassName: (state) => (theme) => theme?.description ?? state.theme.description,
-  getSerializableState: (state) => {
+  getThemeClass:
+    (state, { systemTheme }) =>
+    (theme) => {
+      theme = theme ?? state.theme;
+      return theme === themes.SYSTEM ? systemTheme : theme;
+    },
+  serializableState: (state) => {
     const serializableState = {};
 
-    serializableState.theme = state.theme.description;
+    serializableState.theme = state.theme;
 
     return serializableState;
   },
-  getTheme: (state) => {
-    if (state.theme === themes.SYSTEM) return state.systemTheme;
-    return state.theme;
-  },
-  getSystemTheme: () => () => {
+  theme: (state, { systemTheme }) => (state.theme === themes.SYSTEM ? systemTheme : state.theme),
+  systemTheme: () => {
     if (window.matchMedia('(prefers-contrast: more)').matches) {
       return themes.HIGH_CONTRAST;
     }
