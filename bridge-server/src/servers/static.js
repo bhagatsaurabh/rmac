d5a5a143e5e7b91f9c2ccfd7e0c1e04e72559e7a
Saurabh Bhagat <saurabhbhagat98die@gmail.com>
2023-02-22 23:07:30 +0530

    bridge: /api/hosts

--- bridge-server/src/servers/static.js
+++ bridge-server/src/servers/static.js
@@ -1,13 +1,26 @@
 import { dirname, resolve } from "path";
 import { fileURLToPath } from "url";
 import express from "express";
+import bodyParser from "body-parser";
+import { getHosts } from "../store/store.js";
+import cors from "cors";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const staticServer = express();
+
+if (process.env.PROFILE === "Dev") {
+  staticServer.use(cors());
+}
+
+staticServer.get("/api/hosts", async (_req, res) => {
+  res.send(await getHosts());
+});
+
 staticServer.use(express.static(resolve(__dirname, "../../public")));
 staticServer.get("*", (_req, res) => {
   res.sendFile(resolve(__dirname, "../../public/index.html"));
 });
+staticServer.use(bodyParser.json());
 
 export default staticServer;
--- bridge-server/src/store/store.js
+++ bridge-server/src/store/store.js
@@ -1,3 +1,5 @@
+import { db } from "../config/firebase.js";
+
 const state = {
   hosts: {},
   consoles: {},
@@ -16,5 +18,25 @@ const addConsole = (id, socket) => {
 const removeConsole = (id) => {
   delete state.consoles[id];
 };
+const getHosts = async () => {
+  let hosts = {};
+  Object.keys(state.hosts).forEach(
+    (socketId) => (hosts[socketId] = { health: true, registered: false })
+  );
 
-export { state, addHost, removeHost, addConsole, removeConsole };
+  const snap = await db.ref().once("value");
+  if (snap.exists()) {
+    const registeredHosts = snap.val();
+    Object.keys(registeredHosts).forEach((id) => {
+      if (hosts[id]) {
+        hosts[id] = { ...hosts[id], ...registeredHosts[id], registered: true };
+      } else {
+        hosts[id] = { ...registeredHosts[id], health: false, registered: true };
+      }
+    });
+  }
+
+  return Object.keys(hosts).map((id) => ({ ...hosts[id], id }));
+};
+
+export { state, addHost, removeHost, addConsole, removeConsole, getHosts };
